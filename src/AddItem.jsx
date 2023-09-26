bb94215f59e46082c6388a31a4f5e0d2d25cdd53
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2023-09-26 17:23:08 -0300

    Making all work. We can now submit new items.

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -2,30 +2,25 @@ import { RxPlus } from "react-icons/rx";
 import { useRef } from 'react';
 
 /*
-const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
-
-    <form className='addForm' onSubmit={handleSubmit}>
-
     ref={inputRef}
-
-    value={newItem}
-    onChange={(e) => setNewItem(e.target.value)}
     
     onClick={() => inputRef.current.focus()}
 */
 
-const AddItem = () => {
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
     const inputRef = useRef();
 
     return (
-        <form className='addForm'>
-            <label htmlFor='addItem'>Add Item</label>
+        <form className='addForm' onSubmit={handleSubmit}>
+        <label htmlFor='addItem'>Add Item</label>
             <input
                 autoFocus
                 id='addItem'
                 type='text'
                 placeholder='Add Item'
                 required
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
             />
             <button
                 type='submit'
